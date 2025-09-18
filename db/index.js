c903168dff4545f12e67da30121c41f3d2571c69
Naman Muley <naman.g.muley@gmail.com>
2025-09-18 10:33:21 +0530

    Add db/index.js

---
+++ db/index.js
@@ -0,0 +1,9 @@
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:5432/inventorydb'
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+  pool
+};
